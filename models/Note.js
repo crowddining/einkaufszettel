bb6b92d23336601e55c437058d836fa6481c5936
timsuchanek <tim.suchanek@gmail.com>
2014-03-23 11:41:43 +0100

    clean, ready for presentation

--- models/Note.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var noteSchema = new mongoose.Schema({
-	item: {type: String, required: true},
-	amount: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Note', noteSchema);
